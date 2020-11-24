47e18787bfda745397d187affdd7113c7682dbfc
sn0wd3er <69336817+sn0wd3er@users.noreply.github.com>
2020-11-24 23:50:55 +0900

    Change Fonts
    change Fonts

--- clock.js
+++ clock.js
@@ -12,29 +12,29 @@ function getTime() {
 
     switch (day) {
         case 0:
-            day = "일";
+            day = "Sunday";
             break;
         case 1:
-            day = "월";
+            day = "Monday";
             break;
         case 2:
-            day = "화";
+            day = "Tuesday";
             break;
         case 3:
-            day = "수";
+            day = "Wednesday"
             break;
         case 4:
-            day = "목";
+            day = "Thursday";
             break;
         case 5:
-            day = "금";
+            day = "Friday";
             break;
         case 6:
-            day = "토";
+            day = "Saturday";
             break;
     }
 
-    div.innerText = `${month < 10 ? `0${month}`:`${month}`}월 / ${date < 10 ? `0${date}` : `${date}`}일 / ${day}요일
+    div.innerText = `${month < 10 ? `0${month}`:`${month}`} / ${date < 10 ? `0${date}` : `${date}`} / ${day}
     ${hour < 10 ? `0${hour}`:`${hour}`} : ${min < 10 ? `0${min}` : `${min}`} : ${sec < 10 ? `0${sec}` : `${sec}`}`;
 
 }
